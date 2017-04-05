6c6db71f1b18a25841a8a959ec458ddd35da9996
marcel grilo <marcelgrilo86@gmail.com>
2017-04-05 15:02:21 -0300

    configuração do banco de dados e jwt

---
+++ server/config/config.js
@@ -0,0 +1,14 @@
+export default {
+  database: 'ssv-server',
+  username: '',
+  password: '',
+  params: {
+    dialect: 'sqlite',
+    storage: process.env.NODE_ENV ? 'test_ssv.sqlite' : 'ssv.sqlite',
+    define: {
+      underscored: true
+    },
+  },
+  jwtSecret: '$3cr3t'
+  jetSession: { session: false },
+}
---
+++ server/config/datasource.js
@@ -0,0 +1,45 @@
+import Sequelize from 'sequelize';
+import fs from 'fs';
+import path from 'path';
+
+let database = null;
+
+// função para carregar os models dinamicamente no banco.
+const loadModels = (sequelize) => {
+  const dir = path.join(__dirname, '../models');
+  const models = [];
+  fs.readdirSync(dir).forEach((file) => {
+    const modelDir = path.join(dir, file);
+    const model = sequelize.import(modelDir);
+
+    models[model.name] = model;
+  });
+  return models;
+};
+
+export default (app) => {
+  // caso o database nao esteja inicializado
+  if (!database) {
+    const config = app.config;
+    const sequelize = new Sequelize(
+      config.database,
+      config.username,
+      config.password,
+      config.params,
+    );
+
+    database = {
+      sequelize,
+      Sequelize,
+      models: {},
+    };
+
+    database.models = loadModels(sequelize);
+
+    sequelize.sync().done(() =>
+      // isto garante que cada vez que se iniciar a aplicaçao, o database será sincronizado
+       database);
+  }
+  // caso o database ja tenha sido inicializado apenas retorna;
+  return database;
+};
