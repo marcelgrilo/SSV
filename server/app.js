7df0292f13c083f5870cdbc48b5646e23ecf02db
marcel grilo <marcelgrilo86@gmail.com>
2017-04-05 14:53:41 -0300

    modelo do usuario criado

--- server/app.js
+++ server/app.js
@@ -1,9 +1,14 @@
-import express from "express";
+import express from 'express';
+import bodyParser from 'body-parser';
+
+
+
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 
+app.use(bodyParser.json());
 
 app.set('port', port);
 
